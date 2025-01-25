657976c460dcb16a16ba235b06d5507c0773912e
NursultanAuyelkhan <n_auyelkhan@kbtu.kz>
2025-01-25 16:28:36 +0500

    1st

---
+++ 2_while_loops.py
@@ -0,0 +1,17 @@
+i = 1
+while i < 6:
+    print(i)
+    i += 1
+i = 1
+while i < 6:
+    print(i)
+    if i == 3:
+        break
+    i += 1
+
+i = 1
+while i < 6:
+    print(i)
+    i += 1
+else:
+    print("i is no longer than 6")
